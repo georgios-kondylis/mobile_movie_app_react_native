07b8251dd40aadad4726baf965a6958e3e914f77
Georgios <georgios.p.kondylis@gmail.com>
2025-06-26 16:09:13 +0200

    i fixed it there is a very weird bug or glitch that only some of the tailwind classes apply for example some classes would only apply on the <Text> index +1 only if i applied them first in another file in the index.tsx where i display the latest movies makes no sense they dont even have the same parent

--- services/TrendingCard.tsx
+++ services/TrendingCard.tsx
@@ -11,13 +11,13 @@ const TrendingCard = ({movie : {movie_id, title, poster_url}, index} : TrendingC
                    style={{ width: 138, height: 200, borderRadius: 7 }}
                    resizeMode="cover"
             />
-            <View className={`absolute bottom-0 left-0 w-full h-full`}>
-                <Text className="text-xl text-white font-bold mt-2" numberOfLines={1}>
+            <View className={`absolute bottom-7 left-[-10px] left-0 w-full `}>
+                <Text className="text-white text-[4rem] font-bold mt-2" numberOfLines={1}>
                     {index + 1}
                 </Text>
             </View>
 
-            <Text className="text-sm font-bold text-white mt-2" numberOfLines={2}>
+            <Text className="text-2xl font-bold text-white mt-2" numberOfLines={2}>
                 {title}
             </Text>
         </TouchableOpacity>
